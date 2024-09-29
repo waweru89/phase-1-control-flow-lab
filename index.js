f41eea1ea113d021dc9bbe5c5f0401194db5a97d
MBURU <wawerumburu157@gmail.com>
2024-09-29 17:03:31 +0300

    js

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  if (feet<=400){
+  return 'This one is on me!';
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+ if (feet>400&& feet<=2000){
+  return 'That will be twenty bucks.';
+  
+}
+if(feet>2000&& feet<=2500){
+  return 'I will gladly take your thirty bucks.';
+  }else{
+  return 'No can do.';
+}
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+function ternaryCheckCity(city) {
+    if (city === 'NYC') {
+    return 'Ok, sounds good.' }
+    else{
+      return 'No go.';
+    }
+  }
+
+
+function switchOnCharmFromTip(tip){
+  if(tip=== 'generous'){
+    return 'Thank you so much.';
+  }
+  if(tip==='not as generous'){
+    return 'Thank you.'}
+    else{
+      return 'Bye.';
+    }
+  }
+ 
